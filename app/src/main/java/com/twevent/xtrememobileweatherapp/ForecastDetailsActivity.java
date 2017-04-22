29d76a8a9e62087a56384bd9b67f0e0b507db9f6
Rajani Karuturi <rajanikaruturi@gmail.com>
2017-04-22 12:06:39 +0530

    added a fragement on load of activity

--- app/src/main/java/com/twevent/xtrememobileweatherapp/ForecastDetailsActivity.java
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/ForecastDetailsActivity.java
@@ -1,5 +1,7 @@
 package com.twevent.xtrememobileweatherapp;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -13,5 +15,14 @@ public class ForecastDetailsActivity extends AppCompatActivity {
         String cityName = getIntent().getStringExtra("cityName");
         TextView cityTextView = (TextView) findViewById(R.id.detailsCityName);
         cityTextView.setText(cityName);
+        startForecastFragment();
+    }
+
+    private void startForecastFragment() {
+        ForecastFragment fragment = new ForecastFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.forecast_container, fragment);
+        fragmentTransaction.commit();
     }
 }
